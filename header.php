da0773d220e3009816689987a0932f22dbd92e3c
ViniciusOLucio <138497678+ViniciusOLucio@users.noreply.github.com>
2023-08-24 19:17:31 -0300

    refactor: troca de css por sass

--- header.php
+++ header.php
@@ -5,7 +5,7 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Forsaken Dogs</title>
-  <link rel="stylesheet" href="assets/css/style.css">
+  <link rel="stylesheet" href="assets/css/scss/main.css">
 
   <!-- font awersome -->
   <script src="https://kit.fontawesome.com/48a96fe751.js" crossorigin="anonymous"></script>
